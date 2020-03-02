10f996be70862bb596599f71d3655cdfc412877c
Jahirul Islam <jahir.ewubd@gmail.com>
2020-03-01 22:15:56 -0800

    initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { LeftBarComponent } from './left-bar/left-bar.component';
+import { RootComponent } from './root/root.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FacultyComponent,
+    LeftBarComponent,
+    RootComponent
   ],
   imports: [
     BrowserModule
