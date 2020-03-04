46e516c2547007edf11102e84014dcb2d7b84bdb
Jahirul Islam <jahir.ewubd@gmail.com>
2020-03-04 23:26:19 +0600

    admin 2 pages with normal sidebar

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,34 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {AdminDashboardComponent} from './admin-dashboard/admin-dashboard.component';
-import {AdminCourseFacultyComponent} from './admin-course-faculty/admin-course-faculty.component';
+import { AdminComponent } from './admin/admin.component';
+
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { AdminCourseFacultyComponent } from './admin-course-faculty/admin-course-faculty.component';
 
 const routes: Routes = [
     {
-        path: 'admin/dashboard',
-        component: AdminDashboardComponent,
-    }, {
-        path: 'admin/course_faculty',
-        component: AdminCourseFacultyComponent,
-    }, {
-        path: 'admin/student',
-        component: AdminDashboardComponent,
-    }, {
-        path: 'course_offering',
-        component: AdminDashboardComponent,
-    }, {
-        path: 'section_offering',
-        component: AdminDashboardComponent,
-    }, {
-        path: 'copo_mapping',
-        component: AdminDashboardComponent,
-    }, {
-        path: 'reports',
-        component: AdminDashboardComponent,
-    }, {
-        path: 'charts',
-        component: AdminDashboardComponent,
+        path: 'admin',
+        component: AdminComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: AdminDashboardComponent,
+            }, {
+                path: 'course_faculty',
+                component: AdminCourseFacultyComponent,
+            }, {
+                path: 'student',
+                component: AdminDashboardComponent,
+            }, {
+                path: 'course_offering',
+                component: AdminDashboardComponent,
+            }, {
+                path: 'section_offering',
+                component: AdminDashboardComponent,
+            }, {
+                path: 'copo_mapping',
+                component: AdminDashboardComponent,
+            }, {
+                path: 'reports',
+                component: AdminDashboardComponent,
+            }, {
+                path: 'charts',
+                component: AdminDashboardComponent,
+            }
+        ]
     }
 ];
 
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,12 +5,14 @@ import { AdminRoutingModule } from './admin-routing.module';
 
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminCourseFacultyComponent } from './admin-course-faculty/admin-course-faculty.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 @NgModule({
     declarations: [
         AdminDashboardComponent,
-        AdminCourseFacultyComponent
+        AdminCourseFacultyComponent,
+        AdminComponent
     ],
     imports: [
         CommonModule,
