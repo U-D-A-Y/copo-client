484aef102d4e6d264f166f8ff4fe83b526a7b45b
Jahirul Islam <jahir.ewubd@gmail.com>
2020-03-29 00:27:33 +0600

    add a userservice

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -3,11 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
+import { User, UserService} from '../user/user.service';
 @Injectable()
 export class LoginService {
     constructor(
         private http: HttpClient,
-        private router: Router
+        private router: Router,
+        private userService: UserService
     ) {}
 
     proxyPrefix = "/api";
@@ -27,6 +29,7 @@ export class LoginService {
 
                 if (result["success"] === true) {
                     // window.location.href = `${access}/${access}_dashboard`;
+                    this.setUserInfo(result["data"]);
                     this.router.navigate([`/${role}`]);
                 } else {
                     let error = result["error"];
@@ -40,4 +43,16 @@ export class LoginService {
         )       
     }
     
+
+    setUserInfo(data) {
+        let user: User = {
+            name : data["name"],
+            username : data["username"],
+            initial : data["initial"],
+            role : data["role"],
+            designation : data["designation"],
+        }
+
+        this.userService.setUserInfo(user);
+    }
 }
---
+++ src/app/user/user.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    constructor() {}
+    
+    proxyPrefix = "/api";
+
+    getUserInfoFromServer(username) {
+        let apiUrl = this.proxyPrefix + "/"
+    }
+
+    setUserInfo(user: User) {
+        let userString = JSON.stringify(user);
+        localStorage.setItem('copo-user', userString);
+    }
+
+    getUserInfo(): User {
+        let userString = localStorage.getItem('copo-user');
+        let user: User = JSON.parse(userString);
+        return user;
+    }
+
+    updateUserInfo(propertiesToUpdate) {
+        let userString = localStorage.getItem('copo-user');
+        let user: User = JSON.parse(userString);
+        for (let property in propertiesToUpdate) {
+            let value = propertiesToUpdate[property];
+            user[property] = value;
+        }
+        userString = JSON.stringify(user);
+        localStorage.setItem('copo-user', userString);
+    }
+
+    removeUserInfo() {
+        localStorage.setItem("copo-user", "");
+    }
+}
+
+export interface User {
+    username: String,
+    role: String,
+    name: String,
+    designation: String,
+    initial: String
+}
