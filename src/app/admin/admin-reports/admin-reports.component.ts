46983fab70db3a310ffbaf722ff22dff5ea44c75
Jahirul Islam <jahir.ewubd@gmail.com>
2020-03-31 00:35:30 +0600

    add copo mapping grid

--- src/app/admin/admin-reports/admin-reports.component.ts
+++ src/app/admin/admin-reports/admin-reports.component.ts
@@ -1,15 +1,109 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { AdminReportService } from './admin-reports.service';
+import { getStudentPoReport, getCoursePoReport, getCourseAndStudentPoReport} from '@common/colDefs';
 
 @Component({
-  selector: 'app-admin-reports',
-  templateUrl: './admin-reports.component.html',
-  styleUrls: ['./admin-reports.component.css']
+    selector: 'app-admin-reports',
+    templateUrl: './admin-reports.component.html',
+    styleUrls: ['./admin-reports.component.css'],
+    providers: [AdminReportService]
 })
 export class AdminReportsComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private renderer: Renderer2,
+        private service: AdminReportService
+    ) { }
 
-  ngOnInit(): void {
-  }
+    reportColDefs: any;
+    reportRowData: any;
 
+    @ViewChild('reportAgGrid') reportAgGrid: AgGridAngular;
+
+    pillText = "";
+    coursePillTextList = [];
+    studentPillTextList = [];
+
+    ngOnInit(): void {
+
+    }
+
+    @ViewChild('coursePillBox') coursePillBox: ElementRef;
+    @ViewChild('coursePillBoxholder') coursePillBoxholder: ElementRef;
+
+    coursePillBoxKeyUp(event) {
+        if (event.keyCode == 13 || event.keyCode == 32) {
+            this.pillText = this.coursePillBox.nativeElement.value.trim();
+            console.log(this.pillText);
+            this.coursePillBox.nativeElement.value = '';
+            let div = this.createpillBox(this.pillText, this.coursePillTextList, this.coursePillBoxholder.nativeElement);
+            // coursePillBoxholder.appendChild(div);
+            this.renderer.appendChild(this.coursePillBoxholder.nativeElement, div);
+            this.coursePillTextList.push(this.pillText)
+        }   
+    }
+
+    createpillBox(text, array, holder) {
+        let div = this.renderer.createElement('div');
+        div.className = 'parentPillDiv'
+        let span = this.renderer.createElement('span')
+        span.innerText = text;
+        let btn = this.renderer.createElement('button')
+        btn.className = 'btnPillBox'
+        btn.innerText = 'X'
+        btn.addEventListener('click', () => {
+            let ind = array.indexOf(span.textContent);
+            array.splice(ind, 1);
+            // holder.removeChild(div);
+            this.renderer.removeChild(holder, div);
+        })
+        this.renderer.appendChild(div, span);
+        this.renderer.appendChild(div, btn);
+        return div;
+    }
+
+    @ViewChild('studentPillBoxInput') studentPillBox: ElementRef;
+    @ViewChild('studentPillBoxholder') studentPillBoxHolder: ElementRef;
+
+    studentPillBoxKeyUp(event) {
+        if (event.keyCode == 13 || event.keyCode == 32) {
+            this.pillText = this.studentPillBox.nativeElement.value.trim();
+            this.studentPillBox.nativeElement.value = '';
+            let div = this.createpillBox(this.pillText, this.studentPillTextList, this.studentPillBoxHolder.nativeElement);
+            // studentPillBoxholder.appendChild(div);
+            this.renderer.appendChild(this.studentPillBoxHolder.nativeElement, div);
+            this.studentPillTextList.push(this.pillText);
+        }
+    }
+
+    generateReport() {
+        console.log(this.coursePillTextList);
+        console.log(this.studentPillTextList);
+        this.reportRowData = [];
+        if (this.coursePillTextList.length > 0 && this.studentPillTextList.length > 0) {
+            this.reportColDefs = this.getCol('courseAndStudentPo');
+            this.reportRowData = this.service.getCourseAndStudentReport(
+                this.coursePillTextList, this.studentPillTextList
+            )
+        } else if (this.studentPillTextList.length > 0) {
+            this.reportColDefs = this.getCol('studentPo');
+            this.reportRowData = this.service.getStudentReport(this.studentPillTextList);
+        }
+        else if (this.coursePillTextList.length > 0) {
+            this.reportColDefs = this.getCol('coursePo');
+            this.reportRowData = this.service.getCourseReport(this.coursePillTextList);
+        }
+    }
+
+    getCol(type) {
+        switch (type) {
+            case 'studentPo':
+                return getStudentPoReport();
+            case 'coursePo':
+                return getCoursePoReport();
+            case 'courseAndStudentPo':
+                return getCourseAndStudentPoReport();
+        }
+    }
 }
---
+++ src/app/admin/admin-reports/admin-reports.service.ts
@@ -0,0 +1,137 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class AdminReportService {
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    proxy = "/api";
+
+    getCourseAndStudentReport(course, student) {
+        let courseQuery = this.getUrlformArray('course', course);
+        let studentQuery = this.getUrlformArray('student', student);
+        let combinedQuery = `${courseQuery}&${studentQuery}`;
+        let apiURL = this.proxy + `/admin/report/po/course_student/?${combinedQuery}`;
+        return this.http.get(apiURL)
+        .pipe(
+            map(result => {
+                let data = result["data"];
+                return this.courseAndStudentPrepareData(data);
+            })
+        )
+    }
+
+    getStudentReport(student) {
+        let apiUrl = this.proxy + `/admin/report/po/student/?${this.getUrlformArray('student', student)}`
+        return this.http.get(apiUrl)
+        .pipe(
+            map(result => {
+                let data = result["data"];
+                return this.studentPrepareData(data);
+            })
+        )
+    }
+
+    getCourseReport(course) {
+        let apiURL = this.proxy + `/admin/report/po/course/?${this.getUrlformArray('course', course)}`;
+        return this.http.get(apiURL)
+        .pipe(
+            map(result => {
+                let data = result["data"];
+                console.log("data", data);
+                return this.coursePrepareData(data);
+            })
+        )
+    }
+    
+    studentPrepareData(arr) {
+        let data = []
+        for (let i = 0; i < arr.length; i++) {
+            let added = false;
+            for (let j of arr[i].po_dist) {
+                if (!added) {
+                    data.push({
+                        student_id: arr[i].student_id,
+                        student_name: arr[i].student_name,
+                        span: (arr[i].po_dist.length),
+                        ...j
+                    })
+                    added = true;
+                } else {
+                    data.push(j);
+                }
+            }
+        }
+        return data;
+    }
+
+    coursePrepareData(arr) {
+        let data = []
+        for (let i = 0; i < arr.length; i++) {
+            let added = false;
+            for (let j of arr[i].po_dist) {
+                if (!added) {
+                    data.push({
+                        course_code: arr[i].course_code,
+                        span: (arr[i].po_dist.length),
+                        ...j,
+                    })
+                    added = true;
+                } else {
+                    data.push(j);
+                }
+            }
+        }
+        return data;
+    }
+
+    courseAndStudentPrepareData(arr) {
+        let data = [];
+        for (let i = 0; i < arr.length; i++) {
+            let courseAdded = false;
+            console.log(arr[i]);
+            for (let student of arr[i].students) {
+                let studentAdded = false;
+                for (let po of student.po_dist) {
+                    if (!courseAdded) {
+                        data.push({
+                            course_code: arr[i].course_code,
+                            student_id: student.student_id,
+                            student_name: student.student_name,
+                            courseSpan: arr[i].students.length * student.po_dist.length,
+                            studentSpan: student.po_dist.length,
+                            ...po,
+                        });
+                        courseAdded = true;
+                        studentAdded = true;
+                    } else {
+                        if (!studentAdded) {
+                            data.push({
+                                student_id: student.student_id,
+                                student_name: student.student_name,
+                                studentSpan: student.po_dist.length,
+                                ...po,
+                            });
+                            studentAdded = true;
+                        } else {
+                            data.push({
+                                ...po
+                            })
+                        }
+                    }
+                }
+            }
+        }
+        return data;
+    }
+
+    getUrlformArray(type, data) {
+        let url = data.map(item => {
+            return type + '[]=' + item.toUpperCase()
+        })
+        return url.join('&')
+    }
+}
